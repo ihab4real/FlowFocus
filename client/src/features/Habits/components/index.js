7bddf6e7edf2e3452e48994a2d2d3ccc8dd5e2c0
ihab4real <ihabadham4@gmail.com>
2025-05-31 16:55:37 +0300

    feat(client/ht): add analytics ui components

---
+++ client/src/features/Habits/components/index.js
@@ -0,0 +1,19 @@
+// Export all components from their respective directories
+
+// Dashboard components
+export { default as HabitsDashboardPanel } from "./dashboard/HabitsDashboardPanel";
+
+// Form components
+export { default as HabitForm } from "./forms/HabitForm";
+export { default as HabitTemplateSelector } from "./forms/HabitTemplateSelector";
+
+// Tracking components
+export { default as HabitCard } from "./tracking/HabitCard";
+
+// Analytics components
+export { default as StreakDisplay } from "./analytics/StreakDisplay";
+export { default as WeeklyCalendar } from "./analytics/WeeklyCalendar";
+export { default as HabitAnalytics } from "./analytics/HabitAnalytics";
+
+// UI components
+export { default as ProgressRing } from "./ui/ProgressRing";
