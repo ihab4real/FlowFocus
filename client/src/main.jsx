7d6b8f66dc23f2dc9d44462c694bdf857bb14ed7
ihab4real <ihabadham4@gmail.com>
2025-06-06 00:07:59 +0300

    chore(client): set up Toaster component for notifications
    this is why i haven't seen a toast all this time

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import { queryClient } from "./config/queryClient";
 import { router } from "./routes/router";
 import "./index.css";
@@ -10,6 +11,17 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 3000,
+          style: {
+            background: "hsl(var(--background))",
+            color: "hsl(var(--foreground))",
+            border: "1px solid hsl(var(--border))",
+          },
+        }}
+      />
     </QueryClientProvider>
   </StrictMode>
 );
